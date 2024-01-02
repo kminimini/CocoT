de48b9422e9f267e0f4a3c8625fd3b406fe04e4c
gim-yanghyeon <gim-yanghyeon@localhost>
2024-01-03 02:17:09 +0900

    인덱스 수정 완료

--- src/main/java/com/coco/controller/HomeController.java
+++ src/main/java/com/coco/controller/HomeController.java
@@ -8,13 +8,18 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import com.coco.domain.Notice;
+import com.coco.domain.Qna;
 import com.coco.service.NoticeService;
+import com.coco.service.QnaService;
 
 @Controller
 public class HomeController {
 
 	@Autowired
 	private NoticeService noticeService;
+	
+	@Autowired
+	private QnaService qnaService;
 
 	@GetMapping("/")
 	public String showIndexPage(Model model) {
@@ -24,8 +29,13 @@ public class HomeController {
 
 		List<Notice> noticeList = noticeService.getNoticeList(null); // 예시: NoticeService를 통해 공지사항 데이터를 가져옴
 		model.addAttribute("noticeList", noticeList);
+		
+		List<Qna> qnaList = qnaService.getQnaList(null); // 예시: NoticeService를 통해 공지사항 데이터를 가져옴
+		model.addAttribute("qnaList", qnaList);
+
 
 		return "index.html";
 	}
 
+	
 }
