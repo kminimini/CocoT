31a70eeab8dfb78a4a597b076d9220f7da77e7e7
gim-yanghyeon <gim-yanghyeon@localhost>
2024-01-02 11:50:54 +0900

    공지사항 수정 머지x

---
+++ src/main/java/com/coco/controller/NoticeController.java
@@ -0,0 +1,14 @@
+package com.coco.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class NoticeController {
+
+	@GetMapping("/notice")
+	public String notice() {
+		return "/notice";
+	}
+
+}
